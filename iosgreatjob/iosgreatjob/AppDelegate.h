20bfd89f8b1e631a236060e1dbc2591f317ec3e9
Jean-Charles Sisk <jasisk@gmail.com>
2015-04-17 14:29:30 -0400

    init commit

---
+++ iosgreatjob/iosgreatjob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosgreatjob
+//
+//  Created by Jean-Charles Sisk on 4/17/15.
+//  Copyright (c) 2015 Jean-Charles Sisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosgreatjob/iosgreatjob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosgreatjob
+//
+//  Created by Jean-Charles Sisk on 4/17/15.
+//  Copyright (c) 2015 Jean-Charles Sisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
